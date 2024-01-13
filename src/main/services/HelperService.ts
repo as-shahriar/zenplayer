840669dd2eac8d8d450e0b0309ae812ccf0a4d1a
asif <asif.qubit@gmail.com>
2024-01-13 19:47:14 +0600

    Filtering supported video formats and empty folders

--- src/main/services/HelperService.ts
+++ src/main/services/HelperService.ts
@@ -1,5 +1,5 @@
 export class HelperService {
-    static validateMedia(pathname) {
+    static validateMedia(pathname: string) {
         const validFormats = ['.webm', '.3gp', '.m4v', '.mkv', '.mov', '.mp4'];
         return validFormats.some((format) => pathname.endsWith(format));
     }
